f041c26d4d0e655fefbf2565c9791a740f92dee4
Christopher Akinsanmi <christopher.akinsanmi@gmail.com>
2022-08-11 06:04:49 -0700

    Prototype: list_t *add_node(list_t **head, const char *str);

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list.
+ * @str: head of the linked list
+ * @head: the list struct
+ * Return:  the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *value;
+int lenstr;
+
+for (lenstr = 0; str[lenstr] != '\0'; lenstr++)
+;
+
+/* Malloc here? */
+value = malloc(sizeof(list_t));
+if (value == NULL)
+{
+/* if value is NULL return NULL */
+return (NULL);
+free(value);
+}
+
+/* Add a new node */
+value->len = lenstr;
+value->str = strdup(str);
+value->next = *head;
+*head = value;
+/* return pointer */
+return (value);
+free(value);
+}
