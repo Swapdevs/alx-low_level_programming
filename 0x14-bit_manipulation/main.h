b9d1136ad301e7d7ad2015df6bcad1fe1b6b9d08
Christopher Akinsanmi <christopher.akinsanmi@gmail.com>
2022-08-19 00:05:12 +0100

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-unsigned long int _power(unsigned int base, unsigned int pow);
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -10,4 +15,4 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 int _putchar(char c);
 
-#endif /*MAIN_H*/
+#endif
